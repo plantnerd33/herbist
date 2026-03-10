43f6ba5b266c27e3150e51a95d274dad1b4c79cf
Monika Fekete <plantnerd33@gmail.com>
2026-03-10 19:31:36 -0400

    Add form to enter herbs.

--- lib/Herbist.pm
+++ lib/Herbist.pm
@@ -44,6 +44,88 @@ get '/' => sub {
     };
 };
 
+# GET route to show the add herb form
+get '/add' => sub {
+    template 'add_herb' => { 'title' => 'Add New Herb' };
+};
+
+# POST route to process the add herb form
+post '/add' => sub {
+    my $params = body_parameters;
+    
+    eval {
+        database->quick_insert('herb', {
+            common_name => $params->get('common_name'),
+            latin_name  => $params->get('latin_name'),
+            description => $params->get('description'),
+            synonyms    => $params->get('synonyms'),
+            other_names => $params->get('other_names'),
+            collection  => $params->get('collection'),
+            uses        => $params->get('uses'),
+            safety      => $params->get('safety'),
+            dosage      => $params->get('dosage'),
+        });
+    };
+
+    if ($@) {
+        warning "Failed to insert herb: $@";
+        return template 'add_herb' => { 
+            'title' => 'Add New Herb',
+            'error' => 'Failed to save herb to database.' 
+        };
+    }
+
+    redirect '/';
+};
+
+# GET route to show herb details and manage supporting data
+get '/herb/:id' => sub {
+    my $id = route_parameters->get('id');
+    my $db = database;
+    
+    my $herb = $db->quick_select('herb', { ID => $id });
+    if (!$herb) {
+        send_error("Herb not found", 404);
+    }
+
+    my $data = {
+        herb => $herb,
+        constituents => $db->selectall_arrayref("SELECT * FROM constituents WHERE herb_id = ?", { Slice => {} }, $id),
+        actions      => $db->selectall_arrayref("SELECT * FROM actions WHERE herb_id = ?", { Slice => {} }, $id),
+        energetics   => $db->selectall_arrayref("SELECT * FROM energetics WHERE herb_id = ?", { Slice => {} }, $id),
+        indications  => $db->selectall_arrayref("SELECT * FROM indications WHERE herb_id = ?", { Slice => {} }, $id),
+        plantfamily  => $db->selectall_arrayref("SELECT * FROM plantfamily WHERE herb_id = ?", { Slice => {} }, $id),
+        bodysystems  => $db->selectall_arrayref("SELECT * FROM bodysystems WHERE herb_id = ?", { Slice => {} }, $id),
+    };
+
+    template 'manage_herb' => $data;
+};
+
+# POST route to add data to supporting tables
+post '/herb/:id/add_data' => sub {
+    my $id = route_parameters->get('id');
+    my $table = body_parameters->get('table');
+    my $name = body_parameters->get('name');
+
+    my @allowed_tables = qw(constituents actions energetics indications plantfamily bodysystems);
+    if (!grep { $_ eq $table } @allowed_tables) {
+        send_error("Invalid table", 400);
+    }
+
+    eval {
+        database->quick_insert($table, {
+            herb_id => $id,
+            name    => $name,
+        });
+    };
+
+    if ($@) {
+        warning "Failed to insert into $table: $@";
+    }
+
+    redirect "/herb/$id";
+};
+
 # POST route to handle image uploads for a specific herb
 post '/upload/:id' => sub {
     my $id = route_parameters->get('id');
